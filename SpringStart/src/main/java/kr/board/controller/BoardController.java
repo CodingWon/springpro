d379fbfc3e2f79eb2e48843d30a8b7565ade7a87
CodingWon <kingcoding2298@gmail.com>
2023-01-29 09:03:05 +0900

    jackson-databind api , ResponseBody Controller

--- SpringStart/src/main/java/kr/board/controller/BoardController.java
+++ SpringStart/src/main/java/kr/board/controller/BoardController.java
@@ -3,18 +3,32 @@ package kr.board.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.board.entity.Board;
+import kr.board.mapper.BoardMapper;
 
 
 @Controller
 public class BoardController {
 
+	@Autowired
+	BoardMapper boardMapper;
+	
 	@RequestMapping("/")
 	public String main() {
 		return "main";
+	}
+	
+	//@ResponseBody -> jackson-databind (객체-> JSON 데이터 포맷 변환)
+	@RequestMapping("/boardList.do")
+	public @ResponseBody List<Board> boardList(){
+		List<Board> list = boardMapper.getLists();
+		return list;
 	}
 	
 }
