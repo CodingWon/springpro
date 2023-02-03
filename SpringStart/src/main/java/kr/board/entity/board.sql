cecd40ba17788f8bd3ac032c90e17d6bdcf452de
CodingWon <kingcoding2298@gmail.com>
2023-02-03 21:01:24 +0900

    Board entity

--- SpringStart/src/main/java/kr/board/entity/board.sql
+++ SpringStart/src/main/java/kr/board/entity/board.sql
@@ -11,8 +11,23 @@ create table myboard (
 insert into myboard(title,content,writer,indate)
 values ("ㅎㅇㅎㅇ","헬로우","자바",now())
 
+insert into myboard(title,content,writer,indate)
+values ("ㅎㅇ","헬로우","자바",now())
+
 
 select * from myboard
+
+create table mem_tbl(
+	memIdx int auto_increment,
+	memID varchar(20) not null,
+	memPassword varchar(20) not null,
+	memName varchar(20) not null,
+	memAge int,
+	memGender varchar(20),
+	memEmail varchar(50),
+	memProfile varchar(50),
+	primary key(memIdx)
+);
 
 
 
