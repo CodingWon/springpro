dd43a659a3446d50b248cffcc888d3a386c9f488
CodingWon <kingcoding2298@gmail.com>
2023-02-03 21:39:39 +0900

    templete header

--- SpringStart/src/main/java/kr/board/controller/MainController.java
+++ SpringStart/src/main/java/kr/board/controller/MainController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BoardController {
+public class MainController {
 
-	@RequestMapping("/boardMain.do")
-	public String main() {
-		return "board/main";
+	@RequestMapping("/")
+	public String index() {
+		return "index"; 
 	}
 }
--- SpringStart/src/main/webapp/WEB-INF/views/board/main.jsp
+++ SpringStart/src/main/webapp/WEB-INF/views/board/main.jsp
@@ -172,7 +172,7 @@
   </script>
 </head>
 <body>
- 
+ <jsp:include page="../common/header.jsp" />
 <div class="container">
   <h2>Spring 02</h2>
   <div class="panel panel-default">
